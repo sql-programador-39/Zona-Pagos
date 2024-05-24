06be6e3990f0a9f5c047f457c75cbb01e2f81eec
sql-programador-39 <sql.programador39@opa.com.co>
2024-05-24 16:52:12 -0500

    cosas

--- src/context/ConfigProvider.jsx
+++ src/context/ConfigProvider.jsx
@@ -1,15 +1,118 @@
 import { createContext, useState } from 'react'
-
+import axios from 'axios'
 
 const ConfigContext = createContext()
 
 const ConfigProvider = ({children}) => {
 
-  const [infoConfig, setInfoConfig] = useState({})
+  const [clientId, setClientId] = useState('')
+  const [clientName, setClientName] = useState('')
+  const [companyId, setCompanyId] = useState('')
+  const [commerceId, setCommerceId] = useState('')
+  const [bankId, setBankId] = useState('')
+  const [password, setPassword] = useState('')
+  const [searchReferences, setSearchReferences] = useState({})
+  const [paymentReferences, setPaymentReferences] = useState({})
+  const [paymentPlaceId, setPaymentPlaceId] = useState("")
+  const [provider, setProvider] = useState(0)
+
+  const getConfig = async () => {
+
+    const url = 'http://localhost:9090/api/ZOpaOperations/ConfiguracionGeneral'
+
+    try {
+      const response = await axios.post(url)
+      const data = await response.data.object
+
+      setClientId(data.jsonGeneralConfiguration.clientId)
+      setClientName(data.jsonGeneralConfiguration.clientName)
+      setCompanyId(data.companyId)
+      setCommerceId(data.paymentPlacesConfigurations[0].commerceId)
+      setBankId(data.paymentPlacesConfigurations[0].bankId)
+      setPassword(data.paymentPlacesConfigurations[0].password)
+      setSearchReferences(data.paymentPlacesConfigurations[0].searchReferencesService)
+      setPaymentReferences(data.paymentPlacesConfigurations[0].paymentReferenceService)
+      setPaymentPlaceId(data.paymentPlacesConfigurations[0].paymentPlaceId)
+      setProvider(data.paymentPlacesConfigurations[0].provider)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+
+    const updatedObject = {
+      "commerceId": commerceId,
+      "bankId": bankId,
+      "password": password,
+      "searchReferencesService": searchReferences,
+      "paymentReferenceService": paymentReferences
+    }
+
+    const escaped = escapeJsonString(updatedObject)
+    sendInfo(escaped)
+  }
+
+  const sendInfo = async (escaped) => {
+    const url = 'http://localhost:9090/api/ZOpaOperations/ActualizarConfiguracionGeneral'
+
+    try {
+      const response = await axios.post(url, { 
+        "jsonGeneralConfiguration": {
+          "clientId": clientId,
+          "clientName": clientName
+        },
+        "paymentPlacesConfiguration": [
+          {
+            "paymentPlaceId": paymentPlaceId,
+            "provider": Number(provider),
+            "unserializedConfiguration": escaped
+          }
+        ] 
+      }
+    )
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const escapeJsonString = (jsonObject) => {
+    const jsonString = JSON.stringify(jsonObject, null, 2);
+    return jsonString
+      .replace(/</g, '\\u003c')
+      .replace(/>/g, '\\u003e')
+      .replace(/&/g, '\\u0026')
+      .replace(/'/g, '\\u0027');
+  };
 
   return (
     <ConfigContext.Provider value={{
-      setInfoConfig,
+      getConfig,
+      handleSubmit,
+      clientId,
+      clientName,
+      companyId,
+      commerceId,
+      bankId,
+      password,
+      searchReferences,
+      paymentReferences,
+      paymentPlaceId,
+      provider,
+      setClientId,
+      setClientName,
+      setCompanyId,
+      setCommerceId,
+      setBankId,
+      setPassword,
+      setSearchReferences,
+      setPaymentReferences,
+      setPaymentPlaceId,
+      setProvider
     }}>
       {children}
     </ConfigContext.Provider>
--- src/pages/Config/Config.jsx
+++ src/pages/Config/Config.jsx
@@ -1,101 +1,310 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useConfig from '../../hooks/useConfig'
+import { Switch } from 'antd'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
-
-
-import Alert from '../../components/Alert/Alert'
 import { style } from './styleConfig'
 
 const Config = () => {
 
-  const [idCommerce, setIdCommerce] = useState('')
-  const [client, setClient] = useState('')
-  const [service, setService] = useState('')
-  
+  const {
+    getConfig,
+    handleSubmit,
+    clientId,
+    clientName,
+    companyId,
+    commerceId,
+    bankId,
+    password,
+    searchReferences,
+    paymentReferences,
+    paymentPlaceId,
+    provider,
+    setClientId,
+    setClientName,
+    setCompanyId,
+    setCommerceId,
+    setBankId,
+    setPassword,
+    setSearchReferences,
+    setPaymentReferences,
+    setPaymentPlaceId,
+    setProvider
+  } = useConfig()
 
-  const [configAlert, setConfigAlert] = useState(false)
+  const [checked, setChecked] = useState(false)
 
+  useEffect(() => {
+    getConfig()
+  }, [])
 
-  const { setInfoConfig } = useConfig()
-
-  const handleSubmitConfig = (e) => {
-    e.preventDefault()
-
-    if([idCommerce, client, service].includes('')) {
-        setConfigAlert(true)
-      setTimeout(() => {
-        setConfigAlert(false)
-      }, 4000)
-      return
-    }
-
-    setInfoConfig({ idCommerce, client, service })
-  }
+  const onChange = (checked) => {
+    setChecked(checked)
+  };
 
   return (
     <>
       <section>
-        <h1 className={ style.h1 }>Configuración</h1>
 
-        <div className="my-8 md:my-10">
-          <form action="" className='grid lg:grid-cols-2 gap-5 grid-cols-1'>
-            <div className={ `${ style.divInputConfig }` }>
-              <label htmlFor="id-commerce" className={ style.label }>IdComercio:</label>
-              <input 
-                type="text" 
-                className={` ${style.input} mt-3 sm:m-0`} 
-                name='id-commerce' 
-                id='id-commerce'
-                placeholder='Id del comercio'
-                value={ idCommerce }
-                onChange={ e => {
-                  setIdCommerce(e.target.value)
-                }}
-              />
-            </div>
-
-            <div className={ `${ style.divInputConfig }` }>
-              <label htmlFor="client" className={ style.label }>Cliente:</label>
-              <input 
-                type="text" 
-                className={` ${style.input} mt-3 sm:m-0 `}
-                name='client' 
-                id='client'
-                placeholder='Nombre del cliente'
-                value={ client }
-                onChange={ e => {
-                  setClient(e.target.value)
-                }}
-              />
-            </div>
-
-            <div className={ `${ style.divInputConfig }` }>
-              <label htmlFor="service" className={ style.label }>Servicio:</label>
-              <input 
-                type="text" 
-                className={` ${ style.input } mt-3 sm:m-0 `} 
-                name='service' 
-                id='service'
-                placeholder='Nombre del servicio'
-                value={ service }
-                onChange={ e => {
-                  setService(e.target.value)
-                }}
-              />
-            </div>
-
-            <button 
-              type="submit" 
-              className={` ${ style.button } mx-auto w-3/4 lg:w-full lg:m-0 gl:w-1/4` }
-              onClick={ handleSubmitConfig }
-            ><FontAwesomeIcon icon={ faCircleCheck } /> Aplicar cambios</button>
-
-            { configAlert && <Alert msg="Para aplicar la configuración todos los campos deben estar completos" />}
-            
-          </form>
+        <div className='flex justify-between'>
+          <h1 className={ style.h1 }>Configuración</h1>
+          
+          <div className='flex items-center gap-3'>
+            <p className={ style.h1 }>Activar Edición</p>
+            <Switch value={checked} onChange={onChange} />
+          </div>
         </div>
+
+        <form action="" onSubmit={handleSubmit}>
+          <div className="my-3 md:my-5">
+
+
+            <section className='grid grid-cols-3 gap-4'>
+              <div className=''>
+                <label htmlFor='companyId' className='font-medium text-lg'>ID de la compañia (CompanyId):</label>
+                <input 
+                  type="text"
+                  value={companyId}
+                  id='companyId'
+                  name='companyId' 
+                  className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700' 
+                  onChange={(e) => setCompanyId(e.target.value)}
+                  disabled={!checked}
+                />
+              </div>
+
+              <div>
+                <label htmlFor='clientId' className='font-medium text-lg'>ID del cliente (ClientId):</label>
+                <input 
+                  type="text"
+                  value={clientId}
+                  id='clientId'
+                  name='clientId'
+                  className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                  onChange={(e) => setClientId(e.target.value)}
+                  disabled={!checked}
+                />
+              </div>
+
+              <div>
+                <label htmlFor='clientName' className='font-medium text-lg'>Nombre del cliente (ClientName):</label>
+                <input
+                  type="text"
+                  value={clientName}
+                  id='clientName'
+                  name='clientName'
+                  className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700' 
+                  onChange={(e) => setClientName(e.target.value)}
+                  disabled={!checked}
+                />
+              </div>
+            </section>
+
+
+            <div className="my-3 md:my-5">
+              <h3 className='font-bold text-2xl mb-5'>paymentPlacesConfigurations</h3>
+
+              <section className='grid grid-cols-2 gap-8'>
+
+                <div>
+                  <div className='mb-3'>
+                    <label htmlFor='provider' className='font-medium text-lg my-3'>Proveedor (Provider):</label>
+                    <select 
+                      name="provider" 
+                      id="provider" 
+                      value={provider || "0"} 
+                      className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                      onChange={(e) => setProvider(e.target.value)}
+                      disabled={true}
+                    >
+                      <option value="0">Zona Pagos</option>
+                      <option value="1">Efecty</option>
+                    </select>
+                  </div> 
+
+                  <div className='my-3'>
+                    <label htmlFor='commerceId' className='font-medium text-lg'>ID del comercio (CommerceId):</label>
+                    <input
+                      type="text"
+                      value={commerceId}
+                      id='commerceId'
+                      name='commerceId'
+                      className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                      onChange={(e) => setCommerceId(e.target.value)}
+                      disabled={!checked}
+                    />
+                  </div>
+
+                  <div className='my-3'>
+                    <label htmlFor='bankId' className='font-medium text-lg my-3'>ID del banco (BankId):</label>
+                    <input
+                      type="text"
+                      value={bankId}
+                      id='bankId'
+                      name='bankId'
+                      className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                      onChange={(e) => setBankId(e.target.value)}
+                      disabled={!checked}
+                    />
+                  </div>
+                </div>
+                
+                <div>
+                  <div className='mb-3'>
+                    <label htmlFor='password' className='font-medium text-lg my-3'>Contraseña (Password):</label>
+                    <input 
+                      type="text"
+                      value={password}
+                      id='password'
+                      name='password'
+                      className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                      onChange={(e) => setPassword(e.target.value)}
+                      disabled={!checked}
+                    />
+                  </div>
+
+                  <div className='my-3'>
+                    <label htmlFor='paymentPlaceId' className='font-medium text-lg'>ID del lugar de pago (PaymentPlaceId):</label>
+                    <input
+                      type="text"
+                      value={paymentPlaceId}
+                      id='paymentPlaceId'
+                      name='paymentPlaceId'
+                      className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                      onChange={(e) => setPaymentPlaceId(e.target.value)}
+                      disabled={!checked}
+                    />
+                  </div>
+                </div>
+              </section>
+
+
+
+              <section className='grid grid-cols-2 gap-8 mt-5'>
+                <div>
+                  <h3 className='font-bold text-2xl mb-3'>searchReferencesService</h3>
+                  <div>
+                    <div className='my-3'>
+                      <label htmlFor='serviceUrlS' className='font-medium text-lg'>Url del servicio (ServiceUrl):</label>
+                      <input
+                        type="text"
+                        value={searchReferences.serviceUrl || ''}
+                        id='serviceUrlS'
+                        name='serviceUrlS'
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        onChange={(e) => setSearchReferences({...searchReferences, serviceUrl: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='supportLazyLoadingS' className='font-medium text-lg'>Soporte lazy loading (supportLazyLoading):</label>
+                      <select 
+                        name='supportLazyLoadingS'
+                        id='supportLazyLoadingS' 
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        value={paymentReferences.supportLazyLoading || false}
+                        onChange={(e) => setPaymentReferences({...paymentReferences, supportLazyLoading: e.target.value})}
+                        disabled={!checked}
+                      >
+                        <option value={false}>No</option>
+                        <option value={true}>Si</option>
+                      </select>
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='userS' className='font-medium text-lg'>Usuario (User):</label>
+                      <input 
+                        type="text"
+                        value={searchReferences.user || ''}
+                        id='userS'
+                        name='userS'
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        onChange={(e) => setSearchReferences({...searchReferences, user: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='passwordS' className='font-medium text-lg'>Contraseña (Password):</label>
+                      <input
+                        type="text"
+                        value={searchReferences.password || ''}
+                        id='passwordS'
+                        name='passwordS'
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        onChange={(e) => setSearchReferences({...searchReferences, password: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className='font-bold text-2xl mb-3'>paymentReferenceService</h3>
+                  <div>
+                    <div className='my-3'>
+                      <label htmlFor='serviceUrlP' className='font-medium text-lg'>Url del servicio (ServiceUrl):</label>
+                      <input 
+                        type="text" 
+                        value={paymentReferences.serviceUrl || ''} 
+                        id='serviceUrlP' 
+                        name='serviceUrlP' 
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700' 
+                        onChange={(e) => setPaymentReferences({...paymentReferences, serviceUrl: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='supportLazyLoadingP' className='font-medium text-lg'>Soporte lazy loading (supportLazyLoading):</label>
+                      <select 
+                        name='supportLazyLoadingP' 
+                        id='supportLazyLoadingP' 
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        value={paymentReferences.supportLazyLoading || false}
+                        onChange={(e) => setPaymentReferences({...paymentReferences, supportLazyLoading: e.target.value})}
+                        disabled={!checked}
+                      >
+                        <option value={false}>No</option>
+                        <option value={true}>Si</option>
+                      </select>
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='userP' className='font-medium text-lg'>Usuario (User):</label>
+                      <input 
+                        type="text" 
+                        value={paymentReferences.user || ''} 
+                        id='userP' 
+                        name='userP' 
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        onChange={(e) => setPaymentReferences({...paymentReferences, user: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+
+                    <div className='my-3'>
+                      <label htmlFor='passwordP' className='font-medium text-lg'>Contraseña (Password):</label>
+                      <input 
+                        type="text" 
+                        value={paymentReferences.password || ''} 
+                        id='passwordP' 
+                        name='passwordP' 
+                        className='bg-blue-100 w-full mt-2 ps-2 py-2 rounded-lg outline-blue-700'
+                        onChange={(e) => setPaymentReferences({...paymentReferences, password: e.target.value})}
+                        disabled={!checked}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+
+          <div>
+            <button className='bg-blue-800 text-white font-bold px-6 py-2 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline' disabled={!checked} type='submit'>Actualizar datos</button>
+          </div>
+        </form>
       </section>
     </>
   )
